7b874c73aad03ab8373162fbe61d1b1e4a929540
Niket Yende <niket6@gmail.com>
2023-05-31 08:58:58 +0000

    feat: challenge 1 completed

--- packages/basics/main.go
+++ packages/basics/main.go
@@ -1,5 +1,26 @@
 // packages/basics/main.go
 package main
 
+import (
+	"errors"
+	"fmt"
+)
+
+func divide(a, b int) (int, error) {
+	if b == 0 {
+		return 0, errors.New("Cannot divide by 0")
+	}
+	return a / b, nil
+}
+
 func main() {
+	fmt.Println("Hello Niket!")
+
+	// fmt.Printf("Today is %s", time.Now().Weekday())
+
+	// Invoke divide function
+	fmt.Println(divide(10, 2))
+
+	// Invide divide function with 0 as denominator to return error
+	fmt.Println(divide(5, 0))
 }
